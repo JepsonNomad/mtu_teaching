f1df3180b1e3578df282467405808100cdc0f74e
Christian John <cjohn@ucdavis.edu>
2026-01-14 14:21:14 -0800

    pushed livecode update

--- scripts/livecode.R
+++ scripts/livecode.R
@@ -8,3 +8,26 @@
 ## https://github.com/JepsonNomad/mtu_teaching/blob/main/scripts/livecode.R
 ## also @ https://tinyurl.com/GreatLakesIceCover
 
+lake_ice <- read.table("https://www.glerl.noaa.gov/data/ice/glicd/daily/sup.txt")
+
+library(tidyverse)
+
+lake_ice_long <- lake_ice %>%
+  mutate(monthday = rownames(.)) %>%
+  pivot_longer(-monthday) %>%
+  mutate(snowYear = str_sub(name, 2, 5),
+         snowYear = as.numeric(snowYear)) %>%
+  separate(monthday, into = c("month", "monthDay")) %>%
+  mutate(monthNum = match(month, month.abb)) %>%
+  mutate(year = ifelse(monthNum > 6, 
+                       snowYear - 1,
+                       snowYear)) %>%
+  mutate(date = lubridate::ymd(paste0(year, "-", month, "-", monthDay))) %>%
+  filter(!is.na(date)) 
+  
+View(lake_ice_long)
+
+lake_ice_long %>%
+  filter(date == as.Date("2012-03-11"))
+lake_ice_long %>%
+  filter(date == as.Date("2014-03-11"))
